b89d17013d61916414ed14de4046c64d2c06775b
bilgramiraza <bilgramiraza@yahoo.com>
2022-06-27 15:48:04 +0530

    Added Ability to add Task to the list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState({currentTask:"", list:[]});
+  const handleChange = (e)=>{
+    setTasks({
+              ...tasks,
+              currentTask:e.target.value
+            });
+  };
+  const addTask = (e)=>{
+    e.preventDefault();
+    setTasks({list:[...tasks.list,
+                    {
+                      id:Date.now(),
+                      task:tasks.currentTask
+                    }
+                   ],
+              currentTask:""});
+  };
   return (
-    <div className="App">
-      <h1>
-        Basic App Setup
+    <div>
+      <h1 className="App">
+        Organizer
       </h1>
+      <form onSubmit={addTask}>
+        <input type="text" 
+               name="tasks" 
+               id="tasks" 
+               placeholder="Enter Task ..." 
+               onChange={handleChange}
+               value={tasks.currentTask}/>
+        <button>Add Task</button>
+      </form>
+      <div className="App">
+        <h3>Tasks</h3>
+        <ul>
+          {
+            tasks.list.map((item)=><li key={item.id}>{item.task}</li>)
+          }
+        </ul>
+      </div>
     </div>
   );
 }
